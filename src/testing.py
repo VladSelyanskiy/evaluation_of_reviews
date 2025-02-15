658fb49f5c03ba1b1f94e1f876877bd692364cc0
VladSelyanskiy <vlad.selyanskiy@mail.ru>
2025-02-15 23:13:48 +0300

    добавлена возможная реализация импорта сохраненной модели

---
+++ src/testing.py
@@ -0,0 +1,49 @@
+from usage_of_models import use_model, tokenize_sentence
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Макет")
+root.geometry("600x400")
+
+root.grid_rowconfigure(index=0, weight=1)
+root.grid_rowconfigure(index=1, weight=1)
+root.grid_rowconfigure(index=2, weight=1)
+root.grid_rowconfigure(index=4, weight=1)
+root.grid_rowconfigure(index=5, weight=1)
+root.grid_rowconfigure(index=6, weight=1)
+root.grid_rowconfigure(index=7, weight=1)
+root.grid_columnconfigure(index=0, weight=1)
+root.grid_columnconfigure(index=1, weight=1)
+
+label = tk.Label(
+    root,
+    text="Нижмите кнопку 'выбрать файл'\n чтобы опознать несколько строк из текстового файла\n",
+    font="Times 15",
+).grid(row=0, sticky=tk.NSEW, column=0)
+
+label = tk.Label(
+    root,
+    text="Или введи текст в ячейку ниже\n",
+    font="Times 20",
+).grid(row=2, sticky=tk.NSEW)
+
+
+text_editor = tk.Text(height=2)
+text_editor.grid(row=4, sticky=tk.NSEW)
+
+
+def read_text():
+    text = text_editor.get("1.0", tk.END)
+    text_editor.delete("1.0", tk.END)
+    answer.insert(tk.END, f"~~~ {use_model(text)} \n")
+
+
+answer = tk.Text(height=2)
+answer.grid(row=7, sticky=tk.NSEW)
+
+next_button = ttk.Button(text="read text", command=read_text)
+next_button.grid(row=5, sticky=tk.NSEW, column=0)
+
+root.mainloop()
---
+++ src/usage_of_models.py
@@ -0,0 +1,32 @@
+import dill
+import string
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+snowball = SnowballStemmer(language="russian")
+russian_stop_words = stopwords.words("russian")
+
+vectorizer = TfidfVectorizer(
+    tokenizer=lambda x: tokenize_sentence(x, remove_stop_words=True), token_pattern=None
+)
+
+classes = {0: "negative", 1: "positive"}
+
+with open(r"src\models\test.pickle", "rb") as file:
+    model = dill.load(file)["model"]
+
+
+def tokenize_sentence(sentence: str, remove_stop_words: bool = True):
+    tokens = word_tokenize(sentence, language="russian")
+    tokens = [i for i in tokens if i not in string.punctuation]
+    if remove_stop_words:
+        tokens = [i for i in tokens if i not in russian_stop_words]
+    tokens = [snowball.stem(i) for i in tokens]
+    return tokens
+
+
+def use_model(text):
+    return classes[model.predict([text])[0]]
